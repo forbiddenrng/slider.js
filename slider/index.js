455781859e2ab116ed059defffaf1ef2ec0cdf9f
Antek <antek.gawron@gmail.com>
2020-11-09 23:36:27 +0100

    add buttons styles

--- slider/index.js
+++ slider/index.js
@@ -32,10 +32,48 @@ class Slider {
     fetch("./slider/config.json")
       .then(response => response.json())
       .then(data => {
-        // console.log(data)
         this.styles = data
+        this.setSliderStyles(false)
       })
-      .catch(e => console.log(e))
+      .catch(e => {
+        console.log(e)
+        this.setSliderStyles(true)
+      })
+  }
+  setSliderStyles(isDefault) {
+    //BUTTONS
+    //colors
+    this.prevButton.style.backgroundColor = `${isDefault ? "#0966ff" : this.styles.styles.buttons.background}`;
+    this.nextButton.style.backgroundColor = `${isDefault ? "#0966ff" : this.styles.styles.buttons.background}`;
+    //prev button size
+    this.prevButton.style.width = `${isDefault ? `60px` : `${this.styles.styles.buttons.width}px`}`;
+    this.prevButton.style.height = `${isDefault ? `60xp` : `${this.styles.styles.buttons.height}px`}`;
+    //next button size
+    this.nextButton.style.width = `${isDefault ? `60px` : `${this.styles.styles.buttons.width}px`}`;
+    this.nextButton.style.height = `${isDefault ? `60xp` : `${this.styles.styles.buttons.height}px`}`;
+    //ARROWS
+    switch (this.styles.styles.buttons.arrows.type) {
+      case "angle": {
+        this.prevButton.childNodes[0].classList.add("fa-chevron-left")
+        this.nextButton.childNodes[0].classList.add("fa-chevron-right")
+        break;
+      }
+      case "double-angle": {
+        this.prevButton.childNodes[0].classList.add("fa-angle-double-left")
+        this.nextButton.childNodes[0].classList.add("fa-angle-double-right")
+        break;
+      }
+      case "caret": {
+        this.prevButton.childNodes[0].classList.add("fa-caret-left")
+        this.nextButton.childNodes[0].classList.add("fa-caret-right")
+        break;
+      }
+      default: {
+        this.prevButton.childNodes[0].classList.add("fa-arrow-left")
+        this.nextButton.childNodes[0].classList.add("fa-arrow-right")
+        break;
+      }
+    }
   }
 
   initializeSlider(array) {
@@ -50,6 +88,7 @@ class Slider {
       this.disableButton(this.prevButton)
     }
     this.getData()
+
   }
 
 
@@ -58,8 +97,8 @@ class Slider {
     rootElement.insertAdjacentHTML("beforeend", `
       <section class="slider__section">
         <img class="slider__image" data-image>
-        <button class="prev__button" data-prev><i class="fas fa-arrow-left"></i></button>
-        <button class="next__button" data-next><i class="fas fa-arrow-right"></i></button>
+        <button class="prev__button" data-prev><i class="fas"></i></button>
+        <button class="next__button" data-next><i class="fas"></i></button>
       </section>
     `)
     this.initializeSlider(array)
